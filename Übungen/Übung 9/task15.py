82d465f5e3626c11918849df08d519de9f9bf580
Jonas <jograb@students.uni-mainz.de>
2018-06-27 12:23:19 +0200

    updated vorlesungen

--- Übungen/Übung 9/task15.py
+++ Übungen/Übung 9/task15.py
@@ -6,23 +6,25 @@ reg = r"\(([\+\-\*\/][0-9]{2,}|[0-9]+)\)"
 # Test again and again
 while True:
     # Get phrase to check
-    phrase = input("Your phrase to check:\n")
+    phrase = input("Your phrase to check:\n").replace(" ", "")
     # Check input agains regEx if satisfy grammar
     result = fullmatch(reg, phrase)
-    print("Correct:", reg is not None)
+    print("Correct:", result is not None)
 
     if result is not None:
-        # Default result - content of brackets
-        result = phrase.strip("()")
         # If operand is in front calculate with this operand
-        if result[0] in ('+', '-', '*', '/'):
+        if result[1] in ('+', '-', '*', '/'):
+            # Catch zero division exception
             try:
-                result = eval(result[0].join(result[1:]))
+                result = eval(result[1].join(result[2:-1]))
             except ZeroDivisionError:
                 result = None
+        # default result is content of brackets
+        else:
+            result = int(phrase.strip("()"))
             
         # Print result
-        print("Result:", int(result))
+        print("Result:", result)
 
     # New line
     print()
